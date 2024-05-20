2e98adfd6f2c0734258babcdcd7974b21b92d5da
unique2721 <uniquehabeshawi@gmail.com>
2024-05-20 09:26:46 -0700

    #

--- src/components/Carousels.vue
+++ src/components/Carousels.vue
@@ -5,6 +5,6 @@
 
 <template>
     <div>
-        <img src="https://rickandmortyapi.com/api/character/avatar/5.jpeg" alt="" class="h-[500px] w-[3500px] rounded-lg">
+        <img src="https://rickandmortyapi.com/api/character/avatar/5.jpeg" alt="" class="rounded-lg mx-2 sm:h-[300px] md:h-[400px] lg:w-[2000px] lg:h-[475px] xl:h-[500px] xl:w-[3000px]">
     </div>
 </template>
