560463141a49ea398cc6ab36d342366af1d75e42
unique2721 <uniquehabeshawi@gmail.com>
2024-05-20 08:21:04 -0700

    #

--- src/Carousell.vue
+++ src/Carousell.vue
@@ -1,31 +1,14 @@
 <template>
     <div>
       <carousel :autoplay="true" :autoplay-timeout="3000"> <!-- Set autoplay to true and specify autoplay timeout -->
-        <slide v-for="(image, index) in images" :key="index">
-          <img :src="image.src" alt="Carousel Image" />
+        <slide>
+          <img src="./images/logo.png.png" alt="Carousel Image" />
         </slide>
       </carousel>
     </div>
   </template>
   
-  <script>
+  <script setup>
   import { Carousel, Slide } from 'vue-carousel';
-  
-  export default {
-    components: {
-      Carousel,
-      Slide,
-    },
-    data() {
-      return {
-        images: [
-          { src: 'path/to/image1.jpg' },
-          { src: 'path/to/image2.jpg' },
-          { src: 'path/to/image3.jpg' },
-          // Add more image objects as needed
-        ]
-      };
-    },
-  };
   </script>
   
