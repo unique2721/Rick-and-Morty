27378ba53cea095582b394b8b2c5631e34c47b1d
unique2721 <uniquehabeshawi@gmail.com>
2024-05-21 00:36:49 -0700

    #

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <script setup>
 const apologise = () => {
-    window.alert('Sorry for the inconvenience that I did not design a figma. Trust me I will design it until next week. Thank you for your patience and support!')
+    window.alert('Sorry for the inconvenience that I did not design a figma yet. Trust me I will design it until next week. Thank you for your patience and support!')
 }
 const date = new Date().getFullYear();
 </script>
