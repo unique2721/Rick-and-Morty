5eaa0cce86de6f7c963c7d6aa0d621f2e5592f3d
unique2721 <uniquehabeshawi@gmail.com>
2024-05-22 01:20:03 -0700

    #

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -10,7 +10,7 @@ const date = new Date().getFullYear();
             <h1 class="mr-2 text-2xl">Github: </h1>
             <img src="../images/gitlogo.png" width="30px" height="30px" alt="git hub logo" class="rounded-full">
         </a>
-          <a href="https://www.figma.com/proto/TPeEDafSEYvuq6d5GWoNJJ/Rick-and-Morty?node-id=1-176&t=D7Gh7x9EdgdePx3n-0&scaling=min-zoom&page-id=0%3A1" target="_blank" class="flex cursor-pointer">
+          <a href="https://www.figma.com/design/TPeEDafSEYvuq6d5GWoNJJ/Rick-and-Morty?node-id=0-1&t=g1xRUVV3Dn1eHpnM-0" target="_blank" class="flex cursor-pointer">
             <h1 class="text-3xl mr-2">Figma: </h1>
             <img src="../images/figmalogo.png" width="40px" height="25px" alt="figma logo" class="rounded-full">
           </a>
