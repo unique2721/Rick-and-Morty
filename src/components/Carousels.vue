e3fc1db20b30bc1d103807492b9f049f05543301
unique2721 <uniquehabeshawi@gmail.com>
2024-05-24 11:03:46 -0700

    #

--- src/components/Carousels.vue
+++ src/components/Carousels.vue
@@ -10,7 +10,7 @@ const images = ref([
 <template>
     <div>
         <img src="https://rickandmortyapi.com/api/character/avatar/5.jpeg" alt=""
-            class=" mx-2 sm:h-[300px] sm:w-[1000px] md:h-[400px] lg:w-[2000px] lg:h-[475px] xl:h-[500px] xl:w-[3000px]">
+            class="rounded-lg mx-2 sm:h-[300px] sm:w-[1000px] md:h-[400px] lg:w-[2000px] lg:h-[475px] xl:h-[500px] xl:w-[3000px]">
         <!--  <Carousel :autoplay="true">
             <Slide v-for="(image,index) in images" :key="index" class="flex">
                 <div class="e">
