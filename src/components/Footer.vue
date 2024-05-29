90905cb7d098812c9a39d732ccc0f7926fe9c8d7
MEKIB <atlawmekibib@gmail.com>
2024-05-29 10:55:18 -0700

    #

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,12 +8,12 @@ const date = new Date().getFullYear();
       <p class="text-3xl mr-5 italic">&copy; {{ date }} Gebremariam Kidane</p>
       <a href="https://github.com/unique2721/Rick-and-Morty" target="_blank" class="flex mr-10">
         <h1 class="mr-2 text-2xl">Github:</h1>
-        <img src="../images/gitlogo.png" width="30px" height="30px" alt="git hub logo" class="rounded-full" />
+        <img src="../images/gitlogo.png" width="30px" height="30px" alt="git hub logo" class="rounded-full sm:hidden md:block" />
       </a>
       <a href="https://www.figma.com/design/TPeEDafSEYvuq6d5GWoNJJ/Rick-and-Morty?node-id=0-1&t=g1xRUVV3Dn1eHpnM-0"
         target="_blank" class="flex cursor-pointer">
         <h1 class="text-3xl mr-2">Figma:</h1>
-        <img src="../images/figmalogo.png" width="40px" height="25px" alt="figma logo" class="rounded-full" />
+        <img src="../images/figmalogo.png" width="40px" height="25px" alt="figma logo" class="rounded-full sm:hidden md:block"/>
       </a>
     </div>
   </div>
